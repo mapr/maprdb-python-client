d2285d27e94a1a4a39b3d1db2f27fe06c047c7db
dshylov <dshylov@mapr.com>
2018-03-02 17:28:07 +0200

    Added changes into setup.py, setup.cfg and README files

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from os import path
 from pip.req import parse_requirements
 
 here = path.abspath(path.dirname(__file__))
-install_reqs = parse_requirements('requirements.txt', session=False)
-reqs = [str(ir.req) for ir in install_reqs]
+# install_reqs = parse_requirements('requirements.txt', session=False)
+# reqs = [str(ir.req) for ir in install_reqs]
 setup(name='maprdb_python_client',
       version='0.1',
       description='MapR-DB Python Client',
@@ -14,7 +14,7 @@ setup(name='maprdb_python_client',
       author_email='dshylov@mapr.com',
       keywords='ojai python client mapr maprdb',
       packages=find_packages(exclude=['test*', 'docs*', 'examples*']),
-      install_requires=reqs,
+      install_requires=['aenum==2.0.10', 'grpcio==1.9.1', 'grpcio-tools==1.9.1', 'ojai-python-api==0.1', 'python-dateutil==2.6.1', 'regex==2018.2.8'],
       python_requires='>=2.7'
       )
 
