6d1bc602dff39dcef821e41661fb2a6b3f61f628
dshylov <dshylov@mapr.com>
2018-05-25 19:22:11 +0300

    Updated build version

--- setup.py
+++ setup.py
@@ -3,22 +3,18 @@ from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
-# install_reqs = parse_requirements('requirements.txt', session=False)
-# reqs = [str(ir.req) for ir in install_reqs]
-# Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='maprdb_python_client',
-      version='0.1',
+      version='1.0.18052501',
       description='MapR-DB Python Client',
-      url='https://github.com/mapr/maprdb-python-client/tree/dev',
-      author='MapR inc.',
-      author_email='dshylov@mapr.com',
+      url='https://github.com/mapr/maprdb-python-client/',
+      author='MapR, Inc.',
       keywords='ojai python client mapr maprdb',
       packages=find_packages(exclude=['test*', 'docs*', 'examples*']),
       install_requires=['aenum>=2.0.10', 'grpcio==1.9.1', 'grpcio-tools==1.9.1', 'ojai-python-api>=1.0',
                         'python-dateutil>=2.6.1', 'retrying>=1.3.3'],
-      python_requires='>=2.7',
+      python_requires='==2.7',
       long_description=long_description
       )
