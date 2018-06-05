47382b3811e01a18dff1304d47671fe6f809fcc7
dshylov <dshylov@mapr.com>
2018-06-05 12:54:31 +0300

    fix python version

--- setup.py
+++ setup.py
@@ -15,6 +15,6 @@ setup(name='maprdb_python_client',
       packages=find_packages(exclude=['test*', 'docs*', 'examples*']),
       install_requires=['aenum>=2.0.10', 'grpcio==1.9.1', 'grpcio-tools==1.9.1', 'ojai-python-api>=1.0',
                         'python-dateutil>=2.6.1', 'retrying>=1.3.3'],
-      python_requires='==2.7',
+      python_requires='==2.7.*',
       long_description=long_description
       )
