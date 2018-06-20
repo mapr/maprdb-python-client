76a73d91ded9ac42abf0724c8ec2104f7a3e3295
dshylov <dshylov@mapr.com>
2018-06-20 20:08:28 +0300

    Change grpc version in setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='maprdb_python_client',
       author='MapR, Inc.',
       keywords='ojai python client mapr maprdb',
       packages=find_packages(exclude=['test*', 'docs*', 'examples*']),
-      install_requires=['aenum>=2.0.10', 'grpcio==1.9.1', 'grpcio-tools==1.9.1', 'ojai-python-api>=1.0',
+      install_requires=['aenum>=2.0.10', 'grpcio>=1.9.1', 'grpcio-tools>=1.9.1', 'ojai-python-api>=1.0',
                         'python-dateutil>=2.6.1', 'retrying>=1.3.3'],
       python_requires='==2.7.*',
       long_description=long_description
