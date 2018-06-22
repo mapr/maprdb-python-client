c1bc76d657c66fc9a315b8cf2895179c18daa9eb
dshylov <dshylov@mapr.com>
2018-06-22 15:29:43 +0300

    MAPRDB-1279: Use Python Dictionary for Query options
    Closes #40.

--- examples/find_and_find_by_id/Sample3.py
+++ examples/find_and_find_by_id/Sample3.py
@@ -5,9 +5,9 @@ from mapr.ojai.storage.ConnectionFactory import ConnectionFactory
 """Create a connection, get store, insert new document into store"""
 # create a connection
 connection_string = "localhost:5678?auth=basic;user=mapr;password=mapr;" \
-          "ssl=true;" \
-          "sslCA=/opt/mapr/conf/ssl_truststore.pem;" \
-          "sslTargetNameOverride=node.mapr.com"
+                    "ssl=true;" \
+                    "sslCA=/opt/mapr/conf/ssl_truststore.pem;" \
+                    "sslTargetNameOverride=node.mapr.com"
 connection = ConnectionFactory.get_connection(connection_str=connection_string)
 
 # Get a store and assign it as a DocumentStore object
@@ -16,16 +16,19 @@ if connection.is_store_exists(store_path='/find_sample_store1'):
 else:
     document_store = connection.create_store(store_path='/find_sample_store1')
 
-query_condition = connection.new_condition()\
-    .or_()\
-    .is_('address.street', QueryOp.EQUAL, '350 Hoger Way')\
-    .is_('address.street', QueryOp.EQUAL, '38 De Mattei Court')\
+query_condition = connection.new_condition() \
+    .or_() \
+    .is_('address.street', QueryOp.EQUAL, '350 Hoger Way') \
+    .is_('address.street', QueryOp.EQUAL, '38 De Mattei Court') \
     .is_('address.zipCode', QueryOp.EQUAL, 95196).close().close().build()
 
-query = connection.new_query().select(['address.street', 'address.zipCode']).where(query_condition).build().query_dict()
+query = connection.new_query().select(['address.street', 'address.zipCode'])\
+    .where(query_condition).build().query_dict()
 
-query_result = document_store.find(query, timeout=1)
+options = {
+    'ojai.mapr.query.timeout-milliseconds': 1000,
+}
+query_result = document_store.find(query, options=options)
 
 for doc in query_result:
     print(doc)
-
