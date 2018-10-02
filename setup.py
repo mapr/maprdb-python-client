d2abb0b3394f3d99dcb15033f314fc614a5bf024
dshylov <dshylov@mapr.com>
2018-10-02 12:19:41 +0300

    MAPRDB-1297 [Python Client] Adapt the maprdb-python-client for python3
    Initial commit.
    Create branch.

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(name='maprdb_python_client',
       packages=find_packages(exclude=['test*', 'docs*', 'examples*']),
       install_requires=['aenum>=2.0.10', 'grpcio>=1.9.1', 'grpcio-tools>=1.9.1', 'ojai-python-api>=1.0',
                         'python-dateutil>=2.6.1', 'retrying>=1.3.3'],
-      python_requires='==2.7.*',
       long_description='A simple, lightweight library which provides access to MapR-DB.'
                        ' Client library able to supports all existing OJAI functionality'
                        ' and being absolutely capable with Java OJAI connector,'
