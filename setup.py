d0c8a0915afb012d731436f2094fd77dbff6ffdc
dshylov <dshylov@mapr.com>
2018-11-26 15:08:15 +0200

    Change ojay-python-api required version from 1.0 to 1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='maprdb_python_client',
       author='MapR, Inc.',
       keywords='ojai python client mapr maprdb',
       packages=find_packages(exclude=['test*', 'docs*', 'examples*']),
-      install_requires=['aenum>=2.0.10', 'grpcio>=1.9.1', 'grpcio-tools>=1.9.1', 'ojai-python-api>=1.0',
+      install_requires=['aenum>=2.0.10', 'grpcio>=1.9.1', 'grpcio-tools>=1.9.1', 'ojai-python-api>=1.1',
                         'python-dateutil>=2.6.1', 'retrying>=1.3.3', 'future>=0.16.0'],
       python_requires='>=2.7.*',
       long_description='A simple, lightweight library which provides access to MapR-DB.'
