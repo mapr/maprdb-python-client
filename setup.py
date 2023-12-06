3311e3f76a9a25db38025b08b33ffc50f5ad8bf0
Aditya Kishore <adi@hpe.com>
2023-12-05 22:17:58 -0800

    Setting next version to 1.1.7

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 
 setup(name='maprdb_python_client',
-      version='1.1.6',
+      version='1.1.7',
       description='MapR-DB Python Client',
       url='https://github.com/mapr/maprdb-python-client/',
       author='MapR, Inc.',
       keywords='ojai python client mapr maprdb',
       packages=find_packages(exclude=['test*', 'docs*', 'examples*']),
+	  setup_requires=['wheel'],
       install_requires=['aenum>=2.0.10', 'grpcio>=1.9.1', 'grpcio-tools>=1.9.1', 'ojai-python-api>=1.1',
                         'python-dateutil>=2.6.1', 'retrying>=1.3.3', 'future>=0.16.0'],
       python_requires='>=2.7.*',
